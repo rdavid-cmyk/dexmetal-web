a712c15a7fa9d42f27bbaed8931e86d259ca2b89
Richard David <rdavid@gvoltt.com>
2026-04-05 15:47:23 +0000

    checkpoint: before homepage and ancillary pages build

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260402_193639 from './20260402_193639';
+
+export const migrations = [
+  {
+    up: migration_20260402_193639.up,
+    down: migration_20260402_193639.down,
+    name: '20260402_193639'
+  },
+];
