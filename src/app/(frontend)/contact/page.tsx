503345933c241e71a885002efbf975adc2757b85
Richard David <rdavid@gvoltt.com>
2026-04-08 23:43:25 +0000

    replace at-a-glance with series info card

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -107,7 +107,6 @@ export default function ContactPage() {
               </a>
             </p>
             <p>
-              <span className="text-white font-medium">Location:</span> 7100 Redmiles Rd, Laurel MD 20707
             </p>
             <p>
               <span className="text-white font-medium">Company:</span> DexMetal LLC
