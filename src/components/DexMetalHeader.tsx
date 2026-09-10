472502ac12b3a4ab561c77a2df10d3aaa7854aa4
Richard David <rdavid@gvoltt.com>
2026-09-10 00:22:48 +0000

    fix: enlarge and crop header logo

--- src/components/DexMetalHeader.tsx
+++ src/components/DexMetalHeader.tsx
@@ -6,14 +6,16 @@ export function DexMetalHeader() {
       <div className="container py-4">
         <div className="flex items-center justify-between">
           <Link href="/" aria-label="DexMetal home" className="shrink-0">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/images/dexmetal-logo.png"
-              alt="DexMetal"
-              width={2508}
-              height={627}
-              className="h-auto w-28 sm:w-32"
-            />
+            <span className="relative block h-7 w-[7.875rem] overflow-hidden sm:h-8 sm:w-[8.75rem]">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/images/dexmetal-logo.png"
+                alt="DexMetal"
+                width={2508}
+                height={627}
+                className="absolute left-[-0.65rem] top-1/2 h-auto w-[9.45rem] max-w-none -translate-y-1/2 sm:left-[-0.7rem] sm:w-[10.8rem]"
+              />
+            </span>
           </Link>
 
           <nav className="hidden items-center gap-6 text-sm font-body font-medium md:flex">
